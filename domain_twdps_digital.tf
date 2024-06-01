b703a1f1272a81b06a5bc584f14e8c92acee3d72
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-06-01 15:30:14 -0500

    [nc] test pipeline
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- domain_twdps_digital.tf
+++ domain_twdps_digital.tf
@@ -4,7 +4,7 @@ locals {
 
 provider "aws" {
   alias  = "domain_twdps_digital"
-  region = "us-east-2"
+  region = "us-east-1"
   assume_role {
     role_arn = "arn:aws:iam::${var.nonprod_account_id}:role/${var.assume_role}"
   }
