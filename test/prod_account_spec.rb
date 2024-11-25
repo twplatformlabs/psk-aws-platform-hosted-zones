aa94ffde79d0cb03648c9cb2f701155e4e8e1507
Nic Cheneweth <nchenewe@thoughtworks.com>
2024-11-25 13:00:18 -0600

    [nc] minor cleanup
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

---
+++ test/prod_account_spec.rb
@@ -0,0 +1,37 @@
+require 'awspec'
+
+describe route53_hosted_zone('twdps.io.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('prod-i01-aws-us-east-2.twdps.io.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('prod-i01-aws-us-east-2.twdps.digital.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('dev.twdps.io.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('dev.twdps.digital.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('qa.twdps.io.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('qa.twdps.digital.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('api.twdps.io.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('api.twdps.digital.') do
+  it { should exist }
+end
