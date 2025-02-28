025596535e4c722b89ae7c709eeac19e5e5c580f
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-02-28 11:23:06 -0600

    [nc] add tests for new domains and zones
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- test/nonprod_account_spec.rb
+++ test/nonprod_account_spec.rb
@@ -1,5 +1,22 @@
 require 'awspec'
 
+describe route53_hosted_zone('sbx-i01-aws-us-east-1.twplatformlabs.org.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('sbx-i01-aws-us-east-1.twplatformlabs.link.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('preview.twplatformlabs.org.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('preview.twplatformlabs.link.') do
+  it { should exist }
+end
+
+# ===================================================== Deprecated
 describe route53_hosted_zone('twdps.digital.') do
   it { should exist }
 end
--- test/prod_account_spec.rb
+++ test/prod_account_spec.rb
@@ -1,5 +1,46 @@
 require 'awspec'
 
+describe route53_hosted_zone('twplatformlabs.org.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('twplatformlabs.link.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('prod-i01-aws-us-east-2.twplatformlabs.org.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('prod-i01-aws-us-east-2.twplatformlabs.link.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('dev.twplatformlabs.org.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('dev.twplatformlabs.link.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('qa.twplatformlabs.org.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('qa.twplatformlabs.link.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('api.twplatformlabs.org.') do
+  it { should exist }
+end
+
+describe route53_hosted_zone('api.twplatformlabs.link.') do
+  it { should exist }
+end
+
+# ===================================================== Deprecated
 describe route53_hosted_zone('twdps.io.') do
   it { should exist }
 end
