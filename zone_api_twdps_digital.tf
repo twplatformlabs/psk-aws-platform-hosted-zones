d08d24f2532455d9e157f01d7bc928aff3b0e5d7
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-04-25 16:46:49 -0500

    [nc] remove all but one deprecated delegations, prior to removing provider definitions
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- zone_api_twdps_digital.tf
+++ zone_api_twdps_digital.tf
@@ -11,50 +11,50 @@ provider "aws" {
 }
 
 # create a route53 hosted zone for the subdomain in the account defined by the provider above
-# module "subdomain_api_twdps_digital" {
-#   source  = "terraform-aws-modules/route53/aws//modules/zones"
-#   version = "5.0.0"
-#   create  = true
+module "subdomain_api_twdps_digital" {
+  source  = "terraform-aws-modules/route53/aws//modules/zones"
+  version = "5.0.0"
+  create  = true
 
-#   providers = {
-#     aws = aws.subdomain_api_twdps_digital
-#   }
+  providers = {
+    aws = aws.subdomain_api_twdps_digital
+  }
 
-#   zones = {
-#     "api.${local.domain_twdps_digital}" = {
-#       tags = {
-#         cluster = "prod-i01-aws-us-east-2"
-#       }
-#     }
-#   }
+  zones = {
+    "api.${local.domain_twdps_digital}" = {
+      tags = {
+        cluster = "prod-i01-aws-us-east-2"
+      }
+    }
+  }
 
-#   tags = {
-#     pipeline = "psk-aws-platform-hosted-zones"
-#   }
-# }
+  tags = {
+    pipeline = "psk-aws-platform-hosted-zones"
+  }
+}
 
 # Create a zone delegation in the top level domain for this subdomain
-# module "subdomain_zone_delegation_api_twdps_digital" {
-#   source  = "terraform-aws-modules/route53/aws//modules/records"
-#   version = "5.0.0"
-#   create  = true
+module "subdomain_zone_delegation_api_twdps_digital" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "5.0.0"
+  create  = true
 
-#   providers = {
-#     aws = aws.domain_twdps_digital
-#   }
+  providers = {
+    aws = aws.domain_twdps_digital
+  }
 
-#   private_zone = false
-#   zone_name = local.domain_twdps_digital
-#   records = [
-#     {
-#       name            = "api"
-#       type            = "NS"
-#       ttl             = 172800
-#       zone_id         = data.aws_route53_zone.zone_id_twdps_digital.id
-#       allow_overwrite = true
-#       records         = module.subdomain_api_twdps_digital.route53_zone_name_servers["api.${local.domain_twdps_digital}"]
-#     }
-#   ]
+  private_zone = false
+  zone_name = local.domain_twdps_digital
+  records = [
+    {
+      name            = "api"
+      type            = "NS"
+      ttl             = 172800
+      zone_id         = data.aws_route53_zone.zone_id_twdps_digital.id
+      allow_overwrite = true
+      records         = module.subdomain_api_twdps_digital.route53_zone_name_servers["api.${local.domain_twdps_digital}"]
+    }
+  ]
 
-#   depends_on = [module.subdomain_api_twdps_digital]
-# }
+  depends_on = [module.subdomain_api_twdps_digital]
+}
