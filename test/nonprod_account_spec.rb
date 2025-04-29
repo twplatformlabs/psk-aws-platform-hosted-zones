220c2ac0bcec552dc26bd8425404b460af2aac9d
Nic Cheneweth <nchenewe@thoughtworks.com>
2025-04-29 14:39:47 -0500

    [nc] remove deprecated zones
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- test/nonprod_account_spec.rb
+++ test/nonprod_account_spec.rb
@@ -15,24 +15,3 @@ end
 describe route53_hosted_zone('preview.twplatformlabs.link.') do
   it { should exist }
 end
-
-# ===================================================== Deprecated
-describe route53_hosted_zone('twdps.digital.') do
-  it { should exist }
-end
-
-describe route53_hosted_zone('sbx-i01-aws-us-east-1.twdps.io.') do
-  it { should exist }
-end
-
-describe route53_hosted_zone('sbx-i01-aws-us-east-1.twdps.digital.') do
-  it { should exist }
-end
-
-describe route53_hosted_zone('preview.twdps.io.') do
-  it { should exist }
-end
-
-describe route53_hosted_zone('preview.twdps.digital.') do
-  it { should exist }
-end
